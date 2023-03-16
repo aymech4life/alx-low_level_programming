2f8433111c250ab6433e09c0dd79e53306e2e6b1
AYODEJI <ayodejiife@gmail.com>
2023-03-16 14:45:11 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  *  main - A program that prints a line by making use of puts function
  *  Return:0(successful)
  */
-int main(void);
+int main(void)
 {
-	puts("\"Programming is like building a multiligual puzzle\n");
+	puts("\"Programming is ilike building a multiligual puzzle\n");
 	return (0);
 }
