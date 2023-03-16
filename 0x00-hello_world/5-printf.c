702cb05add5f07f3d8bb76352649efc75d824f4b
AYODEJI <ayodejiife@gmail.com>
2023-03-16 14:19:46 -0700

    pre

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *  main - A program that prints a file by making use of the printf function
+ *  Return: 0 (successful)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+
+}
