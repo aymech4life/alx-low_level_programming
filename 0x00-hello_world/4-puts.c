c80e75d06b9786be1cf11c90682727c62d5e5053
AYODEJI <ayodejiife@gmail.com>
2023-03-16 14:30:51 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  *  main - A program that prints a line by making use of puts function
- *  return:0(successful)
+ *  Return:0(successful)
  */
 int main(void);
 {
 	puts("Programming is like building a multiligual puzzle");
-	return(0);
+	return (0);
 }
